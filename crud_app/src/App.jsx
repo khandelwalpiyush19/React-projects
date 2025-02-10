e9dad934fa1a33dd5fdd1c669dbeccb9e0b53507
khandelwalpiyush19 <piyushkhandelwal482@gmail.com>
2025-02-10 19:44:36 +0530

    add form and table structure for CRUD operations with styling

--- crud_app/src/App.jsx
+++ crud_app/src/App.jsx
@@ -1,11 +1,83 @@
 import './App.css'
 
 function App() {
-   
+
 
   return (
     <>
-      
+      <form action="" id='form'>
+        <h2>CRUD app</h2>
+        <div>
+
+          <label htmlFor="Name">Name</label>
+          <input type="text" placeholder='Enter the name' />
+        </div>
+        <div>
+
+          <label htmlFor="Age">Age</label>
+          <input type="number" placeholder='Enter the age' />
+        </div>
+        <div>
+          <label htmlFor="Subject">Subject</label>
+          <input type="text" placeholder='Enter the Subject' />
+        </div>
+
+        <button type='button' id='btn'> Create</button>
+      </form>
+      <div id='container'>
+    <table id='table'>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Subject</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>John</td>
+                <td>23</td>
+                <td>Maths</td>
+                <td>
+                    <button id='btn'>Edit</button>
+                    <button>Delete</button>
+                </td>
+            </tr>
+        </tbody><tbody>
+            <tr>
+                <td>John</td>
+                <td>23</td>
+                <td>Maths</td>
+                <td>
+                    <button id='btn'>Edit</button>
+                    <button>Delete</button>
+                </td>
+            </tr>
+        </tbody><tbody>
+            <tr>
+                <td>John</td>
+                <td>23</td>
+                <td>Maths</td>
+                <td>
+                    <button id='btn'>Edit</button>
+                    <button>Delete</button>
+                </td>
+            </tr>
+        </tbody><tbody>
+            <tr>
+                <td>John</td>
+                <td>23</td>
+                <td>Maths</td>
+                <td>
+                    <button id='btn'>Edit</button>
+                    <button>Delete</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
     </>
   )
 }
