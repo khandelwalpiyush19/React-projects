9ed2d04b352dca78f5d311546f722f97b55f4c41
khandelwalpiyush19 <piyushkhandelwal482@gmail.com>
2025-02-15 12:01:35 +0530

    refactor ListItems component to integrate item removal and update functionality with Redux

--- crud_app/src/components/ListItems.jsx
+++ crud_app/src/components/ListItems.jsx
@@ -1,17 +1,48 @@
 // src/components/ItemList.js
 import React from 'react';
-import { useSelector } from 'react-redux';
-import Item from './Items';
+import { useSelector,useDispatch } from 'react-redux';
+// import Item from './Items';
+import { removeItem,updateItem } from '../features/itemsSlice';
+// import ../App.css;
 
 const ListItems = () => {
-  const items = useSelector((state) => state.items.items);
+
+  const items = useSelector((state) => state.items);
+  const dispatch = useDispatch();
+
+  const handleRemove = (id) => {
+    dispatch(removeItem(id));
+  };
+  const handleUpdate = (item) => {
+    const updatedItem = {...item,quantity: item.quantity + 1};
+    dispatch(updateItem(updatedItem));
+  };
 
   return (
+    <>
+    <table className='table'>
+      <th>
+        <td>Name</td>
+        <td>Email</td>
+        <td>Phone no.</td>
+      </th>
+      <tr>
+        <td>piyush khandelwal</td>
+        <td>piyush@gmail.com</td>
+        <td>7062111518</td>
+      </tr>
+    </table>
     <ul>
-      {items.map((item) => (
-        <Item key={item.id} item={item} />
+      {items.map((item)=>(
+        <li key={item.id}>
+          <span>{item.name }</span>
+          <span>{item.email }</span>
+          <span>{item.phone }</span>
+          {/* <span>{item.id }</span> */}
+          </li>
       ))}
     </ul>
+      </>
   );
 };
 
